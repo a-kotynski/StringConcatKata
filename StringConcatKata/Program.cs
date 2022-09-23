47031679c9b02de3220aefcb753c8542f2ef3b6f
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-09-23 04:00:33 +0200

    Add project files.

---
+++ StringConcatKata/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace StringConcatKata;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine(Smash());
+        //Smash();
+        Console.WriteLine(Smash2());
+
+        //static string Smash()
+        //{
+        //string[] fragmentedString = { "hello", "world", "this", "is", "great" };
+        //string concatenatedString = string.Empty;
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    concatenatedString = String.Concat(fragmentedString[i]);
+        //    Console.WriteLine(fragmentedString[i]);
+        //}
+        //Console.WriteLine(concatenatedString);
+        //return concatenatedString;
+        //}
+        static string Smash2() // should work, but doesn't on kata page (readme)
+        {
+            string[] fragmentedString = { "hello", "world", "this", "is", "great" };
+            string concatenatedString = string.Join(" ", fragmentedString);
+            return concatenatedString;
+
+        }
+    }
+}
